03d4efddbb350b8256208f0724346f1587740a97
Anthony D. Blaom <anthony.blaom@gmail.com>
2023-08-03 11:03:15 +1200

    fix handling of Static transformers to resolve #37

--- test/attemptors.jl
+++ test/attemptors.jl
@@ -37,4 +37,32 @@ MLJBase.load_path(::Type{<:DummyModel}) = "DummyPackage.Some.Where.Over.The.Rain
     @test outcome == "✓"
 end
 
+struct DummyStatic <: Static end
+MLJBase.transform(::DummyStatic, _, x, y) = hcat(x, y)
+MLJBase.package_name(::Type{<:DummyStatic}) = "DummyPackage"
+MLJBase.load_path(::Type{<:DummyStatic}) = "DummyPackage.Some.Thing.Different"
+
+struct SupervisedTransformer <: Deterministic end
+MLJBase.fit(::SupervisedTransformer, verbosity, X, y) = (42, nothing, nothing)
+MLJBase.predict(::SupervisedTransformer, _, Xnew) = fill(4.5, length(Xnew))
+MLJBase.transform(model::SupervisedTransformer, Θ, Xnew) =
+    predict(model, Θ, Xnew)
+MLJBase.package_name(::Type{<:SupervisedTransformer}) = "DummyPackage"
+MLJBase.load_path(::Type{<:SupervisedTransformer}) =
+    "DummyPackage.Some.Thing.Else"
+
+@testset "operations" begin
+    X = fill(1.2, 10)
+    y = X
+    mach = machine(SupervisedTransformer(), X, y) |> fit!
+    operations, outcome = MLJTestInterface.operations(mach, X, y, throw=true)
+    @test operations == "predict, transform"
+    @test outcome == "✓"
+
+    smach = machine(DummyStatic())
+    operations, outcome = MLJTestInterface.operations(smach, X, y)
+    @test operations == "transform"
+    @test outcome == "✓"
+end
+
 true
